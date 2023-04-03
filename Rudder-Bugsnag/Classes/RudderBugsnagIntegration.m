15455a02229ce545ca8a3a12faf96576ae75dbcc
Pallab Maiti <mynamepallabmaiti@gmail.com>
2023-04-03 20:47:39 +0530

    feat: upgraded Bugsnag(6.26.1) and Rudder(1.12.0) SDK

--- Rudder-Bugsnag/Classes/RudderBugsnagIntegration.m
+++ Rudder-Bugsnag/Classes/RudderBugsnagIntegration.m
@@ -16,7 +16,7 @@
     self = [super init];
     if(self){
         NSString *apiKey = [config objectForKey:@"apiKey"];
-        [Bugsnag startBugsnagWithApiKey:apiKey];
+        [Bugsnag startWithApiKey:apiKey];
         [RSLogger logDebug:@"Initializing Bugsnag SDK"];
     }
     return self;
@@ -25,9 +25,7 @@
 - (void) dump:(RSMessage *)message {
     @try {
         if (message != nil) {
-            dispatch_async(dispatch_get_main_queue(), ^{
-                [self processRudderEvent:message];
-            });
+            [self processRudderEvent:message];
         }
     } @catch (NSException *ex) {
         [RSLogger logError:[[NSString alloc] initWithFormat:@"%@", ex]];
@@ -40,24 +38,21 @@
     if ([type isEqualToString:@"identify"]) {
         NSDictionary *properties = message.context.traits;
         properties = [self filterProperties:properties];
-        if(properties!=nil){
+        if (properties != nil) {
             id name = [properties objectForKey:@"name"];
             id email = [properties objectForKey:@"email"];
-            [[Bugsnag configuration] setUser:message.userId withName:name andEmail:email];
+            [Bugsnag setUser:message.userId withEmail:email andName:name];
             
             for (NSString *key in properties) {
                 NSString *value = [properties objectForKey:key];
-                [Bugsnag addAttribute:key withValue:value toTabWithName:@"user"];
+                [Bugsnag addMetadata:value withKey:key toSection:@"user"];
             }
         }
-    }
-    else if ([type isEqualToString:@"screen"]){
-        [[Bugsnag configuration] setContext:message.event];
-    }
-    else if([type isEqualToString:@"track"]){
+    } else if ([type isEqualToString:@"screen"]) {
+        [Bugsnag leaveBreadcrumbWithMessage:[NSString stringWithFormat:@"Viewed %@ Screen", message.event]];
+    } else if([type isEqualToString:@"track"]) {
         [Bugsnag leaveBreadcrumbWithMessage:message.event];
-    }
-    else {
+    } else {
         [RSLogger logDebug:@"Bugsnag Integration: Message Type not supported"];
     }
 }
@@ -77,7 +72,8 @@
 }
 
 - (void)reset {
-    [Bugsnag clearBreadcrumbs];
+    [Bugsnag clearMetadataFromSection:@"user"];
+    [Bugsnag clearFeatureFlags];
 }
 
 @end
